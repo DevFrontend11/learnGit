789f300b88170b506d429975d5f7afc77316a4c7
DevFrontend11 <117019287+DevFrontend11@users.noreply.github.com>
2023-02-16 20:35:11 +0700

    update2

--- learnGit/my-git/src/component/Component1.js
+++ learnGit/my-git/src/component/Component1.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Component1 = () => {
   //Không ở đây phải là gì gì đó....
-  return <div>Lê Văn Dũng</div>;
+  //Tao vừa vào lại file, cho tao pull code veef
+  return <div>Em trai của Khá Bá Ngô</div>;
 };
 
 export default Component1;
